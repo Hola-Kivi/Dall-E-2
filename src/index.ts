ba2da0254e174ea6e0c93abaf3abf3bd2a9a69ea
Kivi <wbc583741544@gmail.com>
2023-05-08 09:12:38 +0800

    add nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
+import config from './config';
 
 import app from './server';
 
-app.listen(3001, () => {
-  console.log('--hello, this is from server');
+app.listen(config.port, () => {
+  console.log(`--hello, this is from server: ${config.port}`);
 });
