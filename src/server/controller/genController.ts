d0c23b7845422b494bcb5fd45f716bd52cbdf2e8
Kivi <wbc583741544@gmail.com>
2023-05-17 10:51:45 +0800

    truncate the version of api

--- src/server/controller/genController.ts
+++ src/server/controller/genController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 
-// import { Configuration, OpenAIApi } from 'openai';
+// import { Configuration, OpenAIApi } from "openai";
 // const configuration = new Configuration({
 //   apiKey: process.env.OPENAI_API_KEY,
 // });
@@ -10,12 +10,13 @@ export const genImage = async (req: Request, res: Response) => {
   const { prompt, size } = req.body;
   const imageSize =
     size === "small" ? "256x256" : size === "medium" ? "512x512" : "1024x1024";
+
   try {
     // const response = await openai.createImage({
     //   prompt,
     //   n: 1,
     //   size: imageSize,
-    //   response_format: 'b64_json',
+    //   response_format: "b64_json",
     // });
     // const image = res.send(response.data.data[0].url);
     // res.send(response.data.data[0].b64_json);
